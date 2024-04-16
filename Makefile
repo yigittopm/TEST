468d0b1a8e31cd44a0dd18440f9e39f145feaef8
Mert <yigittopm@hotmail.com>
2024-04-16 22:07:31 +0300

    Makefile updated

--- Makefile
+++ Makefile
@@ -5,45 +5,45 @@ BINARY_FILE=binary 		      # Binary file name
 #
 # Removes binary
 clean:
-	go clean
-	rm -rf bin/${BINARY_FILE}
+	@go clean
+	@rm -rf bin/${BINARY_FILE}
 
 # Builds main.go
 build: clean
-	go build -o bin/${BINARY_FILE} ${MAIN_PATH}
+	@go build -o bin/${BINARY_FILE} ${MAIN_PATH}
 
 # First removes the binary file than rebuilds main.go and runs the binary
 run: build
-	./bin/${BINARY_FILE}
+	@./bin/${BINARY_FILE}
 
 # Same as run but with environment variables
 dev-run: build
-	POSTGRES_HOST=localhost POSTGRES_USER=postgres  POSTGRES_PASSWORD=password  POSTGRES_DB=godb  POSTGRES_PORT=5432  POSTGRES_SSLMODE=disable  ./bin/${BINARY_FILE}
+	@POSTGRES_HOST=localhost POSTGRES_USER=postgres  POSTGRES_PASSWORD=password  POSTGRES_DB=godb  POSTGRES_PORT=5432  POSTGRES_SSLMODE=disable  ./bin/${BINARY_FILE}
 
 # Docker compose commands
 #
 # The project starts and ends with environment variables
 dev-down:
-	docker compose -f docker-compose.dev.yaml down
+	@docker compose -f docker-compose.dev.yaml down
 dev-up: dev-down
-	docker compose -f docker-compose.dev.yaml up --build
+	@docker compose -f docker-compose.dev.yaml up --build
 
 # The project starts and ends with this command
 # Removes running composes
 down:
-	docker compose down
+	@docker compose down
 # Starts compose file
 up: down
-	docker compose up --build
+	@docker compose up --build
 
 # Tests the project
 #
 # Tests the project
 test: 
-	go test -v ./...
+	@go test -v ./...
 
 # Swagger docs
 #
 # Swagger docs update
 swag:
-	swag init -g ${MAIN_PATH} -o ./docs  
+	@swag init -g ${MAIN_PATH} -o ./docs  
--- internal/users/test/api_test.go
+++ internal/users/test/api_test.go
@@ -1,16 +1,12 @@
 package usecase
 
 import (
-	"bytes"
 	"database/sql"
-	"encoding/json"
-	"net/http"
 	"testing"
 
 	"github.com/DATA-DOG/go-sqlmock"
 	"github.com/gofiber/fiber/v2"
 	"github.com/yigittopm/wl-auth/internal/users"
-	"github.com/yigittopm/wl-auth/internal/users/dtos"
 	"gorm.io/driver/postgres"
 	"gorm.io/gorm"
 )
@@ -43,56 +39,6 @@ func Setup(t *testing.T) {
 
 func TestRegister(t *testing.T) {
 	Setup(t)
-
-	tests := []struct {
-		description         string
-		route               string
-		method              string
-		expectedCode        int
-		expectedContentType string
-		expectedBody        any
-		body                any
-	}{
-		{
-			description:         "New user registration with valid data",
-			route:               "/users/register",
-			method:              "POST",
-			expectedCode:        200,
-			expectedContentType: "application/json",
-			expectedBody:        nil,
-			body: dtos.RegisterRequest{
-				Email:    "yigittopm@hotmail.com",
-				Username: "yigittopm",
-				Password: "password",
-			},
-		},
-	}
-
-	for _, test := range tests {
-		jsonData, _ := json.Marshal(test.body)
-		req, _ := http.NewRequest(
-			test.method,
-			test.route,
-			bytes.NewBuffer(jsonData),
-		)
-		req.Header.Set("Content-Type", "application/json")
-
-		res, err := app.Test(req, -1)
-		if err != nil {
-			t.Errorf("ERROR: %#v", err)
-		}
-
-		if res.StatusCode != test.expectedCode {
-			t.Errorf("Response Code: %#v != Expected Code: %#v\n", res.StatusCode, test.expectedCode)
-		}
-
-		contentType := res.Header.Get("Content-Type")
-		if contentType != test.expectedContentType && res.StatusCode == 200 {
-			t.Errorf("Content-Type: %#v != %#v", contentType, test.expectedContentType)
-		}
-
-	}
-
 }
 
 func TestLogin(t *testing.T) {
